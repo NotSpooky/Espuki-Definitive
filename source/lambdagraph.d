656f28bae50d6fa0ccc4699d8f9f5f8d74a9bf31
Christian Durán <cduran@symbiotic.net>
2022-09-19 15:34:33 -0600

    Make lambdagraph compile

---
+++ source/lambdagraph.d
@@ -0,0 +1,30 @@
+import value;
+import type;
+import graph;
+import std.algorithm;
+import std.array;
+import std.conv;
+import pegged.grammar;
+
+Node createLambda (ParseTree [] lambdaTree) {
+  // Doing this in O(n) would probably be slower.
+  auto lastReferences = lambdaTree
+    .filter! (child => child.name == "Program.LastReference")
+    .map!(c => c.matches[0][1..$].to!uint)
+    .array ()
+    .sort ();
+  uint last = 0;
+  foreach (lastReference; lastReferences) {
+    // Check that no references are missed and keep track of the last one.
+    if (lastReference == last + 1) {
+      lastReference ++;
+    } else if (lastReference > last + 1) {
+      throw new Exception (text (
+        "Missing _", last + 1, ", but found _", lastReference
+      ));
+    }
+  }
+  return Node (InterpretedValue (
+    Code, Var (lambdaTree)
+  ));
+}
